edd203e6da5ff9942ac022e1ca6a87313138bc23
Jingen Lin <jingen.lin.jl@gmail.com>
2013-09-20 11:08:12 -0400

    fix : 2013-09-20 11:08:12 -0400 Friday

--- commit.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-git add .
-date=`date`
-git commit -am "$date $1"
-git pull
-git push
--- new_issues/app/models/timeline.rb
+++ new_issues/app/models/timeline.rb
@@ -1,2 +1,3 @@
 class Timeline < ActiveRecord::Base
+  belongs_to :timelineable, polymorphic: true
 end
